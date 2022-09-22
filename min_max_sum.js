5dae64cc974695b889095deae841de02ec5fd2ca
NishithKody <113385563+NishithKody@users.noreply.github.com>
2022-09-22 13:01:21 -0400

    Create min_max_sum.js
    https://www.hackerrank.com/challenges/three-month-preparation-kit-mini-max-sum/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-one

---
+++ min_max_sum.js
@@ -0,0 +1,51 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function miniMaxSum(arr) {
+    // Write your code here
+    arr.sort();
+    let min_sum = 0;
+    let max_sum = 0;
+    
+    for(let i =0; i < 5 ; i++){
+        if(i!=4)
+            min_sum = min_sum + arr[i];
+        if(i!=0)
+            max_sum = max_sum + arr[i];    
+    }
+    console.log(min_sum, max_sum)
+
+
+}
+
+function main() {
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    miniMaxSum(arr);
+}
