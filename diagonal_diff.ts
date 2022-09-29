90170acf8640533c7f504da9c6e0e7015fabb43a
NishithKody <113385563+NishithKody@users.noreply.github.com>
2022-09-29 19:50:46 -0400

    Create diagonal_diff.ts
    https://www.hackerrank.com/challenges/three-month-preparation-kit-diagonal-difference/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-two

---
+++ diagonal_diff.ts
@@ -0,0 +1,73 @@
+'use strict';
+
+import { WriteStream, createWriteStream } from "fs";
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+/*
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+function diagonalDifference(arr: number[][]): number {
+    // Write your code here
+    let val1=0;
+    let val2=0;
+    
+    let temp=0;
+    let temp2=arr[0].length-1;
+    
+    for(let i of arr){
+        val1 = val1+ i[temp];
+        temp ++;
+        val2 = val2+ i[temp2];
+        temp2 --;
+    }
+    console.log(val1);
+    console.log(val2);
+    if(val2>val1)
+        return val2-val1;
+    else if(val1>val2)
+        return val1-val2;
+    else
+        return 0;
+}
+
+function main() {
+    const ws: WriteStream = createWriteStream(process.env['OUTPUT_PATH']);
+
+    const n: number = parseInt(readLine().trim(), 10);
+
+    let arr: number[][] = Array(n);
+
+    for (let i: number = 0; i < n; i++) {
+        arr[i] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    }
+
+    const result: number = diagonalDifference(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
